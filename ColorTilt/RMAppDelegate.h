48e722b4549067402b026ef30d4ed35b31d6cc75
Ryan <ryan@ryanjm.com>
2014-01-20 12:42:31 -0700

    Initial Commit

---
+++ ColorTilt/RMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RMAppDelegate.h
+//  ColorTilt
+//
+//  Created by Ryan Mathews on 1/20/14.
+//  Copyright (c) 2014 Ryan Mathews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ColorTilt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ColorTilt
+//
+//  Created by Ryan Mathews on 1/20/14.
+//  Copyright (c) 2014 Ryan Mathews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RMAppDelegate class]));
+    }
+}
